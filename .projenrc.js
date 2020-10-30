d27d5d99a0b54ceee7228e9e6b063c9e50bd9798
Joseph Egan <joseph.s.egan@gmail.com>
2020-10-30 18:47:20 +0000

    Add Project and File
    * Fix readonly option not being passedthrough in .projenrc.js
    * Make __init__.py editable

--- .projenrc.js
+++ .projenrc.js
@@ -10,7 +10,7 @@ const {
 
 class File extends FileBase {
   constructor(project, name, options) {
-    super(project, name);
+    super(project, name, options);
 
     this.content = options && options.content ? options.content : [];
   }
@@ -132,6 +132,7 @@ class PoetryProject extends Project {
 
     new File(this, `${srcDir}/__init__.py`, {
       content: [`__version__ = "${options.version}"`],
+      readonly: false,
     });
 
     new File(this, `${srcDir}/py.typed`);
--- synth_a_py/__init__.py
+++ synth_a_py/__init__.py
@@ -1,1 +1,13 @@
 __version__ = "0.1.0"
+
+from .file import EmptyFile, File, SimpleFile
+from .project import Project
+from .spec import Spec
+
+__all__ = [
+    "EmptyFile",
+    "File",
+    "Project",
+    "SimpleFile",
+    "Spec",
+]
