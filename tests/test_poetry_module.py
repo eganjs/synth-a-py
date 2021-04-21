fd376722d163216b7e7caa5e7f4c0c493f0bd19a
Joseph Egan <joseph.s.egan@gmail.com>
2021-04-21 01:41:27 +0100

    Update test

--- tests/test_poetry_module.py
+++ tests/test_poetry_module.py
@@ -1,13 +1,22 @@
 from pathlib import Path
 from textwrap import dedent
+from typing import List, TypedDict
 
 from synth_a_py import Dir, PoetryModule, auto_synth
-from synth_a_py.poetry.versions import Managed
+from synth_a_py.poetry.versions import DependencyDict, Managed
+
+
+# allows mypy to correctly type check `**` statements
+class CommonMetadata(TypedDict):
+    version: str
+    authors: List[str]
+    license: str
+    dependency_management: DependencyDict
 
 
 def test_python_module_poetry(tmp_path: Path) -> None:
     with auto_synth(tmp_path):
-        common_metadata = {
+        common_metadata: CommonMetadata = {
             "version": "1.0.0",
             "authors": ["Joseph Egan <...>"],
             "license": "MIT",
@@ -27,7 +36,7 @@ def test_python_module_poetry(tmp_path: Path) -> None:
         PoetryModule(
             name="deployment",
             description="Code to deploy lambdas",
-            **common_metadata,  # type: ignore
+            **common_metadata,
             dependencies={},
             dev_dependencies={
                 **common_dev_dependencies,
@@ -38,7 +47,7 @@ def test_python_module_poetry(tmp_path: Path) -> None:
             PoetryModule(
                 name="alpha",
                 description="Alpha lambda",
-                **common_metadata,  # type: ignore
+                **common_metadata,
                 dependencies={
                     **common_dependencies,
                     "dep42": {
@@ -54,7 +63,7 @@ def test_python_module_poetry(tmp_path: Path) -> None:
             PoetryModule(
                 name="beta",
                 description="Beta lambda",
-                **common_metadata,  # type: ignore
+                **common_metadata,
                 dependencies={
                     **common_dependencies,
                     "dep42": {
@@ -68,14 +77,71 @@ def test_python_module_poetry(tmp_path: Path) -> None:
                 },
             )
 
-    file_path = tmp_path / "file"
-    assert file_path.stat().st_mode & 0o777 == 0o444
-    assert file_path.read_text() == dedent(
+    assert (tmp_path / "deployment" / "pyproject.toml").read_text() == dedent(
         """\
-        Lorem ipsum dolor sit amet
-        consectetur adipiscing elit
-        Cras eros purus
-        aliquet ac magna ut
-        fermentum cursus turpis
+        [build-system]
+        requires = [ "poetry-core>=1.0.0",]
+        build-backend = "poetry.core.masonry.api"
+
+        [tool.poetry]
+        name = "deployment"
+        description = "Code to deploy lambdas"
+        version = "1.0.0"
+        authors = [ "Joseph Egan <...>",]
+        license = "MIT"
+
+        [tool.poetry.dev-dependencies]
+        pytest = "^5"
+        """
+    )
+
+    assert (tmp_path / "lambdas" / "alpha" / "pyproject.toml").read_text() == dedent(
+        """\
+        [build-system]
+        requires = [ "poetry-core>=1.0.0",]
+        build-backend = "poetry.core.masonry.api"
+
+        [tool.poetry]
+        name = "alpha"
+        description = "Alpha lambda"
+        version = "1.0.0"
+        authors = [ "Joseph Egan <...>",]
+        license = "MIT"
+
+        [tool.poetry.dependencies]
+        python = "^3.8"
+
+        [tool.poetry.dev-dependencies]
+        pytest = "^5"
+
+        [tool.poetry.dependencies.dep42]
+        version = "1.0.0"
+        extras = [ "blue", "green",]
+        """
+    )
+
+    assert (tmp_path / "lambdas" / "beta" / "pyproject.toml").read_text() == dedent(
+        """\
+        [build-system]
+        requires = [ "poetry-core>=1.0.0",]
+        build-backend = "poetry.core.masonry.api"
+
+        [tool.poetry]
+        name = "beta"
+        description = "Beta lambda"
+        version = "1.0.0"
+        authors = [ "Joseph Egan <...>",]
+        license = "MIT"
+
+        [tool.poetry.dependencies]
+        python = "^3.8"
+        pandas = "1.2.0"
+
+        [tool.poetry.dev-dependencies]
+        pytest = "^5"
+
+        [tool.poetry.dependencies.dep42]
+        version = "1.0.0"
+        extras = [ "blue", "green",]
         """
     )
